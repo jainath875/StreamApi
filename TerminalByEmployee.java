c22861f2fbee405b7ec138e925e6687f972fc023
jainath kumhar <jainathkumhar0@gmail.com>
2025-12-16 17:57:26 +0530

    Collectors.partitioningBy

--- TerminalByEmployee.java
+++ TerminalByEmployee.java
@@ -7,7 +7,7 @@ public class TerminalByEmployee {
     public static void main(String[] args) {
         List<Employee> employees = Arrays.asList(
             new Employee("Alice", "HR", 3000),
-            new Employee("Bob", "IT", 4000),
+            new Employee("Bob", "IT", 6000),
             new Employee("Charlie", "HR", 2000),
             new Employee("Dave", "IT", 10000),
             new Employee("Eve", "Finance", 5000)
@@ -20,5 +20,15 @@ public class TerminalByEmployee {
 		            System.out.println("Department: " + department);
 		            employeeList.forEach(System.out::println);
         });
+
+        Map<Boolean, List<Employee>> partitionedBySalary = employees.stream()
+			.collect(Collectors.partitioningBy(employee -> employee.getSalary() > 5000));
+			
+		
+        System.out.println("Salary greater than 5000 :");
+        partitionedBySalary.get(true).forEach(System.out::println);
+
+        System.out.println("Salary less than or equal to 5000 :");
+        partitionedBySalary.get(false).forEach(System.out::println);
     }
 }
