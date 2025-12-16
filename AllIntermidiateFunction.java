4edf809aa2a82dc0ddeb52b3485da5f696e2d5f5
jainath kumhar <jainathkumhar0@gmail.com>
2025-12-16 13:37:46 +0530

    map

--- AllIntermidiateFunction.java
+++ AllIntermidiateFunction.java
@@ -4,9 +4,17 @@ import java.util.List;
 public class AllIntermidiateFunction {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
-        //fileter
+        // fileter
         numbers.stream()
                 .filter(n -> n % 2 == 0)
+                .forEach(System.out::println);
+
+        // map
+
+        List<String> names = Arrays.asList("alice", "bob", "charlie");
+
+        names.stream()
+                .map(String::toUpperCase)
                 .forEach(System.out::println);
     }
 }
