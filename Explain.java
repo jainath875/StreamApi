84c1e6e54617d21e44b8c035a6062f365d457b3e
jainath kumhar <jainathkumhar0@gmail.com>
2025-12-16 12:40:56 +0530

    chang in message

--- Explain.java
+++ Explain.java
@@ -1,5 +1,5 @@
 public class Explain {
     public static void main(String[] args) {
-        System.out.println("This is an example of a simple Java program of Stream Api.");
+        System.out.println("This is an example of a simple Java program of Stream Api Daily.");
     }
 }
