d6fdf8b5a21ba7da5cf3cbaf2605af3806feb9a8
jainath kumhar <jainathkumhar0@gmail.com>
2025-12-16 13:32:08 +0530

    added Program

---
+++ AllIntermidiateFunction.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class AllIntermidiateFunction {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        //fileter
+        numbers.stream()
+                .filter(n -> n % 2 == 0)
+                .forEach(System.out::println);
+    }
+}
