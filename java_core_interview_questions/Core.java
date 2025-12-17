58086b60b881b2c520163d72b688e4fc61feb796
jainath kumhar <jainathkumhar0@gmail.com>
2025-12-17 14:15:05 +0530

    core java interview questions

---
+++ java_core_interview_questions/Core.java
@@ -0,0 +1,7 @@
+package java_core_interview_questions;
+
+public class Core {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java Core Interview Questions!");
+    }
+}
